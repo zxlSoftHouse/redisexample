998c610fa34c507a13f68d51dba234cd1b3df098
zxl <953519816@qq.com>
2019-09-19 17:49:19 +0800

    第一次提交

---
+++ src/main/java/com/zxl/redisexample/RedisexampleApplication.java
@@ -0,0 +1,13 @@
+package com.zxl.redisexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisexampleApplication.class, args);
+    }
+
+}
