6bc2f2b93e3464f0f7a3a65f479255d95d0e03a6
zxl <953519816@qq.com>
2019-10-16 17:24:11 +0800

    直播

--- src/main/java/com/zxl/redisexample/websocket/live/WebsocketLiveHandler.java
+++ src/main/java/com/zxl/redisexample/websocket/live/WebsocketLiveHandler.java
@@ -1,5 +1,10 @@
 package com.zxl.redisexample.websocket.live;
 
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.StreamMessage;
+import io.lettuce.core.XReadArgs;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisCommands;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
@@ -10,10 +15,12 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 // 接收消息
@@ -79,20 +86,57 @@ public class WebsocketLiveHandler extends TextWebSocketHandler {
         UriComponents uriComponents = UriComponentsBuilder.fromUri(session.getUri()).build();
         String roomId = uriComponents.getQueryParams().getFirst("roomId");
         String userId = uriComponents.getQueryParams().getFirst("userId");
-        // 保存消息到redis
-
+        // 保存消息到redis ？形式是streams 生产者和消费者的模式（现在只能redisClient实现）
+        RedisClient redisClient = RedisClient.create("redis://106.12.38.52:6379"); // 每一次都创建
+        StatefulRedisConnection<String, String> connect = redisClient.connect(); // 肯定会用连接池
+        RedisCommands<String, String> redisCommands = connect.sync();
+        redisCommands.xadd("room:msg:" + roomId, "userId", userId, "content", message.getPayload());
         // 发送消息到某个客户端
-        session.sendMessage(new TextMessage("服务器返回信息11"));
+        // session.sendMessage(new TextMessage("服务器返回信息11"));
+    }
+
+    // 启动系统后，执行
+    @PostConstruct // spring默认单实例，调用一次
+    public void init() {
+        new Thread(() -> {
+            // 保存消息到redis ？形式是streams 生产者和消费者的模式（现在只能redisClient实现）
+            RedisClient redisClient = RedisClient.create("redis://106.12.38.52:6379");
+            StatefulRedisConnection<String, String> connect = redisClient.connect();
+            RedisCommands<String, String> redisCommands = connect.sync();
+            // 指定读取消息，读取不同房间的消息
+            while (true) {
+                try {
+
+                    // 订阅当前服务器上用户对应的房间 --- 所有的房间
+                    Set<String> strings = roomUserMap.keySet();
+                    ArrayList<XReadArgs.StreamOffset<String>> streamOffsets = new ArrayList<>();
+                    for (String roomId : strings) {
+                        streamOffsets.add(XReadArgs.StreamOffset.latest("room:msg:" + roomId));
+                    }
+                    if (streamOffsets.size() > 0) {
+                        List<StreamMessage<String, String>> stream_sms_send = redisCommands.xread(XReadArgs.Builder.block(1000), XReadArgs.StreamOffset.latest("room:msg:1001"));
+                        for (StreamMessage<String, String> stringStringStreamMessage : stream_sms_send) {
+                            recvRedisMessage(stringStringStreamMessage);
+                        }
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     // 推送消息（其他人发的消息，推送到直播间）
     // 触发，如果redis里面有数据,则触发执行
-    public void recvRedisMessage(String data) throws IOException {
+    public void recvRedisMessage(StreamMessage<String, String> data) throws IOException {
         // 推送这条消息
         // 1、获取roomId
         // 2、根据房间找session
-        String roomId = "";
-        String message = "";
+        String roomId = data.getBody().get("roomId");
+        String content = data.getBody().get("content");
+        String userId = data.getBody().get("userId");
+
+        String message = userId + "说:" + content;
         List<WebSocketSession> webSocketSessionList = roomUserMap.get(roomId);
         for (WebSocketSession webSocketSession : webSocketSessionList) {
             webSocketSession.sendMessage(new TextMessage(message));
